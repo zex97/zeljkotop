598a5fda12dbea70b9a9bb73e369e00767a1239b
Zeljko Lukic <zeljko.lukic.97@gmail.com>
2023-04-02 15:00:52 +0200

    init 3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome!
+          Welcome to my website!
         </p>
       </header>
     </div>
